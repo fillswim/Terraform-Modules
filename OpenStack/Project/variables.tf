473218a5de6aafc878768a72ce896d7c165bbb6b
Oleg Filatov <fillswim@gmail.com>
2025-03-16 16:02:45 +0300

    Update Project role

--- OpenStack/Project/variables.tf
+++ OpenStack/Project/variables.tf
@@ -10,12 +10,14 @@ variable "user_password" {
   type = string
 }
 
-variable "image_source_url" {
-  type    = string
-  default = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
-}
+# variable "image_name" {
+#   type    = string
+#   default = "Ubuntu 24.04 LTS"
+# }
 
-variable "image_name" {
-  type    = string
-  default = "Ubuntu 24.04 LTS"
-}
+# variable "image_source_url" {
+#   type    = string
+#   default = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+# }
+
+
