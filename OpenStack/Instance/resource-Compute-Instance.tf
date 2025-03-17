b0ffafdff49be141ed3a85a0e3436b22a8cc03df
Oleg Filatov <fillswim@gmail.com>
2025-03-17 14:01:58 +0300

    Update Instance output

--- OpenStack/Instance/resource-Compute-Instance.tf
+++ OpenStack/Instance/resource-Compute-Instance.tf
@@ -13,3 +13,23 @@ resource "openstack_compute_instance_v2" "instance" {
     fixed_ip_v4 = cidrhost(data.openstack_networking_subnet_v2.private_subnet.cidr, var.starting-host-number + count.index)
   }
 }
+
+output "name" {
+  description = "Names of the instances"
+  value       = openstack_compute_instance_v2.instance[*].name
+}
+
+output "ip" {
+  description = "IP addresses of the instances"
+  value       = openstack_compute_instance_v2.instance[*].network[0].fixed_ip_v4
+}
+
+output "details" {
+  description = "Details of the instances"
+  value = {
+    for instance in openstack_compute_instance_v2.instance :
+    instance.name => instance.network[0].fixed_ip_v4
+  }
+}
+
+
