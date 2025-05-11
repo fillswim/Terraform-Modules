823e37f7f1969099f7f5638cb0ec624982fa582f
Oleg Filatov <fillswim@gmail.com>
2025-05-11 19:30:20 +0300

    Update README to include v3 changes: added proxmox5 node with 6 cores and updated variable sections for v1 and v2.

--- Proxmox/Telmate/3.0.1-rc8/README.md
+++ Proxmox/Telmate/3.0.1-rc8/README.md
@@ -1,8 +1,12 @@
 # 3.0.1-rc8
 
-# v1
+## v1
 * Added fixed_vmid variable (if not 0, then uses it instead of calculated from env)
 * Added fixed_name variable (overrides vm_name + count.index + 1 )
 
-# v2
+## v2
 * Added node_splitting variable (if true, then uses proxmox${1 + count.index} instead of proxmox_node)
+
+## v3
+* Added proxmox5 node (6 cores)
+
---
+++ Proxmox/Telmate/3.0.1-rc8/v3/instance/variable.tf
@@ -0,0 +1,237 @@
+
+variable "start_vmid" {
+  default = {
+    "prod" = 1000
+    "test" = 2000
+  }
+}
+
+variable "cpu_cores" {
+  default = {
+    "proxmox1" = 4
+    "proxmox2" = 6
+    "proxmox3" = 4
+    "proxmox4" = 6
+    "proxmox5" = 6
+  }
+}
+
+# =============================================
+#                     Network
+# =============================================
+
+variable "subnet_octet_1" {
+  type    = number
+  default = 192
+}
+
+variable "subnet_octet_2" {
+  type    = number
+  default = 168
+}
+
+variable "subnet_octet_3" {
+  type    = number
+  default = 2
+}
+
+variable "subnet_octet_4" {
+  type    = number
+  default = 71
+}
+
+variable "subnet_mask" {
+  type    = number
+  default = 24
+}
+
+variable "gateway_octet_4" {
+  type    = number
+  default = 1
+}
+
+variable "vlan" {
+  type    = number
+  default = 0
+}
+
+variable "network_model" {
+  type    = string
+  default = "virtio"
+}
+
+variable "network_bridge" {
+  type    = string
+  default = "vmbr0"
+}
+
+variable "nameserver" {
+  type    = string
+  default = "192.168.2.11 192.168.2.12"
+}
+
+variable "searchdomain" {
+  type    = string
+  default = "fillswim.local"
+}
+
+# ================================================
+#                     VM-s
+# ================================================
+
+variable "count_vms" {
+  type    = number
+  default = 1
+}
+
+variable "env" {
+  type    = string
+  default = "test"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "test-ubuntu"
+}
+
+variable "proxmox_node" {
+  type    = string
+  default = "proxmox3"
+}
+
+variable "clone_vm_image" {
+  type    = string
+  default = "ubuntu-22.04-cloud"
+}
+
+variable "memory" {
+  type    = number
+  default = 4096
+}
+
+variable "onboot" {
+  type    = bool
+  default = false
+}
+
+# type = "serial0"
+variable "vga_type" {
+  type    = string
+  default = "virtio" # Ubuntu
+}
+
+# ================================================
+#                     Disks
+# ================================================
+
+variable "scsihw" {
+  type    = string
+  default = "virtio-scsi-pci" # Ubuntu
+}
+
+variable "bootdisk" {
+  type    = string
+  default = "ide0"
+}
+
+variable "boot_order" {
+  type    = string
+  default = "order=virtio0;ide0;net0" # Ubuntu
+}
+
+variable "cloudinit_disk_slot" {
+  type    = string
+  default = "ide0"
+}
+
+variable "cloudinit_disk_type" {
+  type    = string
+  default = "cloudinit"
+}
+
+variable "cloudinit_disk_storage" {
+  type    = string
+  default = "local-lvm"
+}
+
+variable "disk_slot" {
+  type    = string
+  default = "virtio0" # Ubuntu
+}
+
+variable "disk_type" {
+  type    = string
+  default = "disk"
+}
+
+variable "disk_storage" {
+  type    = string
+  default = "local-lvm"
+}
+
+variable "disk_size" {
+  type    = number
+  default = 50
+}
+
+variable "disk_format" {
+  type    = string
+  default = "raw"
+}
+
+# ===============================================
+#                   Extra Disks
+# ===============================================
+
+variable "extra_disks_count" {
+  type    = number
+  default = 0
+}
+
+variable "extra_disks_size" {
+  type    = number
+  default = 100
+}
+
+variable "extra_disks_type" {
+  type    = string
+  default = "disk"
+}
+
+variable "extra_disks_slot_type" {
+  type    = string
+  default = "scsi"
+}
+
+variable "extra_disks_storage" {
+  type    = string
+  default = "HDD"
+}
+
+variable "extra_disks_format" {
+  type    = string
+  default = "raw"
+}
+
+# ===============================================
+#                   Fixed VMID
+# ===============================================
+
+variable "fixed_vmid" {
+  type    = number
+  default = 0
+}
+
+variable "fixed_name" {
+  type    = string
+  default = ""
+}
+
+# ===============================================
+#                   Node Splitting
+# ===============================================
+
+variable "node_splitting" {
+  type    = bool
+  default = false
+}
